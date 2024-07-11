12bec66763c38a8b6ba8a81c4eaad29caf764323
Josh Smith <jbsmith@hamilton.edu>
2024-07-11 13:58:56 -0600

    Apparently git doesn't commit empty folders

--- models/clear_saves.sh
+++ models/clear_saves.sh
@@ -1,2 +1,6 @@
-rm -rf clean_model
-mkdir clean_model
+rm -rf original_model
+rm -rf poisoned_model
+mkdir original_model
+mkdir poisoned_model
+touch original_model/README.md
+touch poisoned_model/README.md
